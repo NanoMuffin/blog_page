353b5442f35407a89fede7b4a9c5ed6ccf133f1a
NanoMuffin <nanomuffin@yandex.ru>
2022-03-28 03:09:33 +0400

    fixed buttons selection in index js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var selectedProject = 0
+var buttonsArray = document.querySelectorAll(".button_test,button_text");
 
-var project1 = document.querySelector(".button8")
-var project2 = document.querySelector(".button9")
-var project3 = document.querySelector(".button10")
+var project1 = buttonsArray[6]
+var project2 = buttonsArray[7]
+var project3 = buttonsArray[8]
 
-var skill1 =  document.querySelector(".button1")
-var skill2 =  document.querySelector(".button2")
-var skill3 =  document.querySelector(".button3")
-var skill4 =  document.querySelector(".button4")
-var skill5 =  document.querySelector(".button5")
-var skill6 =  document.querySelector(".button6")
+var skill1 =  buttonsArray[0]
+var skill2 =  buttonsArray[1]
+var skill3 =  buttonsArray[2]
+var skill4 =  buttonsArray[3]
+var skill5 =  buttonsArray[4]
+var skill6 =  buttonsArray[5]
 
 var body_selector = document.querySelector("body")
 var textToShrink = document.querySelector(".button_text")
@@ -18,12 +19,11 @@ var textToShrink = document.querySelector(".button_text")
 project1.addEventListener("click", choosep1)
 project2.addEventListener("click", choosep2)
 project3.addEventListener("click", choosep3)
-body_selector.addEventListener("load", shrinkText)
 
 
 function choosep1() {
   selectedProject = 1;
-  project1.classList.toggle("button_click");
+  project1.classList.add("button_click");
 
   if (selectedProject == 1) {
       skill2.classList.add("button_click")
@@ -38,7 +38,7 @@ function choosep1() {
 
 function choosep2() {
   selectedProject = 2;
-  project2.classList.toggle("button_click");
+  project2.classList.add("button_click");
 
 
   if (selectedProject == 2) {
@@ -55,8 +55,8 @@ function choosep2() {
 };
 
 function choosep3() {
-  selectedProject = 3;
-  project3.classList.toggle("button_click");
+  selectedProject = 3
+  project3.classList.add("button_click");
 
 
   if (selectedProject == 3) {
@@ -69,13 +69,7 @@ function choosep3() {
 
     project1.classList.remove("button_click")
     project2.classList.remove("button_click")
-  }
+
+    selectedProject = selectedProject + 1
+  } 
 };
-
-//function shrinkText(){
-  //if (body_selector.style.innerWidth == "800px") {
-    //textToShrink.style.fontSize = "0.6rem";
-  //}
-
-
-}
